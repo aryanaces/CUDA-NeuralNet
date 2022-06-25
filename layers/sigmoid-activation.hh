d76400d30d57f471d0a9c0c8d3550d2e2910c2dc
Aryan Chaudhary <43854758+aryanaces@users.noreply.github.com>
2022-06-25 21:59:30 +0530

    Added Files

---
+++ layers/sigmoid-activation.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "nn-layer.hh"
+
+class SigmoidActivation : public NNLayer {
+    private :
+        Matrix A;
+        Matrix Z;
+        Matrix dZ;
+    
+    public :
+
+        SigmoidActivation(std::string name);
+        ~SigmoidActivation();
+
+        Matrix& forward(Matrix& Z);
+        Matrix& backprop(Matrix& dA, float learning_rate = 0.01);
+
+};
